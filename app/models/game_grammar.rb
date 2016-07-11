c92497df865147695e3dc42df14e1a2e6aec6dae
Ethan Dickinson <ethancdickinson@gmail.com>
2016-07-11 12:13:18 -0700

    Write script for evaluating all possible positions in fox paper scissors game

---
+++ app/models/game_grammar.rb
@@ -0,0 +1,220 @@
+require_relative 'board'
+
+class GameGrammar
+
+  def initialize
+    @moves_map = get_moves_map
+  end
+
+  def get_moves_map
+    coords = get_ordered_coords
+    board = Board.new()
+    moves_map = Hash.new()
+
+    coords.each do |coord|
+      destinations = board.adjacent_spaces(coord)
+      moves_map[get_letter(coords, coord)] = destinations.map { |dest| get_letter(coords, dest) }
+    end
+
+    return moves_map
+  end
+
+  def evaluate_board_position(board_code)
+    legal_moves_count = 0
+
+    active_pieces, passive_pieces = split_pieces(board_code)
+    enemy_map = get_enemy_map(active_pieces, passive_pieces)
+    mobile_pieces = get_mobile_pieces(active_pieces, enemy_map)
+
+    mobile_pieces.each do |piece|
+      enemy = enemy_map[piece]
+      destinations = @moves_map[piece]
+
+      destinations.each do |destination|
+        legal_moves_count += 1 if valid?(destination, active_pieces + passive_pieces, enemy)
+      end
+    end
+
+    legal_moves_count
+  end
+
+  def valid?(destination, pieces, enemy)
+    !occupied?(pieces, destination) && !threatened?(destination, enemy)
+  end
+
+  def split_pieces(board_code)
+    side = board_code[0]
+    pieces = board_code.split('')[1..6]
+
+    if side == "r"
+      return pieces[0..2], pieces[3..5]
+    else
+      return pieces[3..5], pieces[0..2]
+    end
+  end
+
+  def get_enemy_map(active_pieces, passive_pieces)
+    return {
+      active_pieces[0] => passive_pieces[1],
+      active_pieces[1] => passive_pieces[2],
+      active_pieces[2] => passive_pieces[0],
+    }
+  end
+
+  def get_mobile_pieces(active_pieces, enemy_map)
+    threatened_pieces = active_pieces.select do |piece|
+      enemy = enemy_map[piece]
+      threatened?(piece, enemy)
+    end
+
+    case threatened_pieces.length
+      when 0
+        active_pieces
+      when 1
+        threatened_pieces
+      else
+        []
+    end
+  end
+
+  def find_enemy(pieces, i)
+    i+1 < pieces.length ? pieces[i+1] : pieces[0]
+  end
+
+  def threatened?(piece, enemy)
+    @moves_map[enemy].include?(piece)
+  end
+
+  def occupied?(pieces, destination)
+    pieces.include?(destination)
+  end
+
+  def get_letter(coords, position)
+    base = 'a'.ord
+    index = coords.index(position)
+
+    return (base + index).chr
+  end
+
+  def get_ordered_coords
+    [
+      [0,0],
+      [0,2],
+      [0,4],
+      [0,6],
+      [1,1],
+      [1,3],
+      [1,5],
+      [2,0],
+      [2,2],
+      [2,4],
+      [2,6],
+      [3,1],
+      [3,3],
+      [3,5],
+      [4,0],
+      [4,2],
+      [4,4],
+      [4,6],
+      [5,1],
+      [5,3],
+      [5,5],
+      [6,0],
+      [6,2],
+      [6,4],
+      [6,6]
+    ]
+  end
+end
+
+
+# if __FILE__ == $PROGRAM_NAME
+#   puts "Test the grammar"
+
+#   grammar = GameGrammar.new
+
+#   test_positions = [
+#     "rabcdgi",
+#     "babcdgi",
+#     "rabcdgj",
+#     "babcdgj",
+#     "rabcdgk",
+#     "babcdgk",
+#     "rabcdgl",
+#     "babcdgl",
+#     "rabcdgm",
+#     "babcdgm",
+#     "rabcdgn",
+#     "babcdgn",
+#     "rabcdgo",
+#     "babcdgo",
+#     "rabcdgp",
+#     "babcdgp",
+#     "rabcdgq",
+#     "babcdgq",
+#     "rabcdgr",
+#     "babcdgr",
+#     "rabcdgs",
+#     "babcdgs",
+#     "rabcdgt",
+#     "babcdgt",
+#     "rabcdgu",
+#     "babcdgu",
+#     "rabcdgv",
+#     "babcdgv",
+#     "rabcdgw",
+#     "babcdgw",
+#     "rabcdgx",
+#     "babcdgx",
+#     "rabcdgy",
+#     "babcdgy",
+#     "rabcdhe",
+#     "babcdhe"
+#   ]
+
+
+#   expected_outcomes = [
+#     0,
+#     2,
+#     1,
+#     2,
+#     2,
+#     1,
+#     2,
+#     2,
+#     2,
+#     2,
+#     2,
+#     1,
+#     2,
+#     2,
+#     2,
+#     2,
+#     2,
+#     2,
+#     2,
+#     2,
+#     2,
+#     2,
+#     2,
+#     2,
+#     2,
+#     2,
+#     2,
+#     2,
+#     2,
+#     2,
+#     2,
+#     2,
+#     2,
+#     2,
+#     0,
+#     3
+#   ]
+
+#   test_positions.each_with_index do |code, i|
+#     expected = expected_outcomes[i]
+#     actual = grammar.evaluate_board_position(code)
+#     puts "#{expected} vs. #{actual}"
+#   end
+# end
---
+++ app/models/game_solver.rb
@@ -0,0 +1,185 @@
+require_relative 'piece'
+require_relative 'game_grammar'
+
+#expected number of game states: 127,512,000
+#currently solved: 1,280,000
+#would need to make code 100 times faster to run in one night
+
+#1 pretty smart guess:
+#an unknown number of arrays and hash entries
+#are getting thrown into hard to access, non-RAM memory
+#(e.g. compressed memory)
+
+#theory: having the table and perms loaded a little bit at a time
+#(say 1%, since we're hitting 17GB of memory, where 4 or 8 = fine)
+#would make things substantially faster,
+#average of 10 seconds per 10k rather than ~3min
+#factor of x20 speedup
+
+#add'l thought is implementing the "game grammar",
+#e.g. determining how many legal moves pieces have without
+#implementing a Board object
+#(since gen'ing up a bunch of extra arrays, twice, is a mite slow)
+#kind of wanted to do it anyway for the AI speedup
+
+class GameSolver
+  def initialize
+    @start_time = Time.now.getutc
+    @colors = ["red", "blue"]
+    @types = ["rock", "paper", "scissors"]
+    @table = {}
+
+    @grammar = GameGrammar.new
+
+    @coords = get_ordered_coords
+    @pieces = []
+
+    @colors.each do |color|
+      @types.each do |type| 
+        @pieces.push(Piece.new(type, color, [0, 0]))
+      end
+    end
+  end
+
+  def run
+    counter = 0
+
+    @coords.permutation(6).each do |perm|
+      write_position(perm)
+      counter += 1
+
+      if counter % 100000 == 0
+        puts "#{counter} done at #{Time.now.getutc.to_s}"
+        save_file
+        @table = {}
+      end
+    end
+
+    save_file
+  end
+
+  def write_position(array)
+    assign_positions(array)
+
+    @colors.each do |color|
+      key = get_code(color)
+
+      #next if @table.key?(key)
+      value = get_score(key)
+
+      @table[key] = value
+    end
+  end
+
+  def assign_positions(array)
+    6.times do |i|
+      @pieces[i].position = array[i]
+    end
+  end
+
+  def get_score(board_code)
+    @grammar.evaluate_board_position(board_code)
+  end
+
+  def get_code(color)
+    #code is a 7-char string:
+    #1 bit for whose turn
+    #1 letter for each piece's location
+    #format: {turn}{redR}{redP}{redS}{blueR}{blueP}{blueS}
+    string = ""
+
+    string += color == "red" ? 'r' : 'b'
+
+    @pieces.each do |piece|
+      string += get_letter_for_position(piece.position)
+    end
+
+    return string
+  end
+
+  def get_letter_for_position(position)
+    base = 'a'.ord
+    index = @coords.index(position)
+
+    return (base + index).chr
+  end
+
+  def get_ordered_coords
+    [
+      [0,0],
+      [0,2],
+      [0,4],
+      [0,6],
+      [1,1],
+      [1,3],
+      [1,5],
+      [2,0],
+      [2,2],
+      [2,4],
+      [2,6],
+      [3,1],
+      [3,3],
+      [3,5],
+      [4,0],
+      [4,2],
+      [4,4],
+      [4,6],
+      [5,1],
+      [5,3],
+      [5,5],
+      [6,0],
+      [6,2],
+      [6,4],
+      [6,6]
+    ]
+  end
+
+  def read_file_size
+    f = File.open("game_states.txt", "r")
+    lines = f.readlines
+
+    f.close
+
+    return lines.length
+  end
+
+  def load_file
+    table = {}
+    f = File.open("game_states.txt", "r")
+    lines = f.readlines
+
+    lines.each do |line|
+      # if first_line
+      #   next if line != first_line
+      # end
+
+      code, score = line.match(/(\w+)=(\d+)/).captures
+      table[code] = score
+
+      # limit -= 1
+      # break if limit == 0
+    end
+
+    f.close
+    return table
+  end
+
+  def save_file
+    File.open("game_states_#{@start_time.to_s}.txt", 'a') do |file|
+      @table.each do |k, v|
+        file.write("#{k}=#{v}")
+        file.write("\n")
+      end
+
+      file.close
+    end
+  end
+end
+
+if __FILE__ == $PROGRAM_NAME
+  puts "Where she stops, nobody knows!"
+
+  GameSolver.new().run
+
+  puts "Complete!"
+end
