db23bcd678d04e3dd05c82faf15d7dce929d68cb
Ethan Dickinson <ethancdickinson@gmail.com>
2016-07-16 17:14:02 -0700

    Add messages to tutorial

--- app/assets/javascripts/tutorial.js
+++ app/assets/javascripts/tutorial.js
@@ -1,19 +1,23 @@
-var tickMs = 2000;
+var tickMs = 3000;
 
 var runTutorial = function(exampleGames) {
   var gameCounter = 0;
   var moveCounter = 0;
 
-  tickTutorial(exampleGames, gameCounter, moveCounter);
+  displayMessage(buildMessage("<span>Demo Game 1:</span>"));
+
+  setTimeout(function() {
+    tickTutorial(exampleGames, gameCounter, moveCounter);
+  }, tickMs / 2)
 }
 
 var tickTutorial = function (exampleGames, gameCounter, moveCounter) {
   var exampleGame = exampleGames[gameCounter];
-  executeTutorialStep(exampleGame[moveCounter]);
+  executeTutorialStep(exampleGame.moves[moveCounter]);
+  publishTutorialMessage(exampleGame.messages[moveCounter]);
   moveCounter++;
 
-  if (moveCounter >= exampleGame.length) {
-    //declareVictory();
+  if (moveCounter >= exampleGame.moves.length) {
     moveCounter = 0;
     gameCounter++;
 
@@ -23,11 +27,12 @@ var tickTutorial = function (exampleGames, gameCounter, moveCounter) {
 
     setTimeout(function() {
       setPiecesToPosition(startingPosition)
+      displayMessage(buildMessage("<span>Demo Game " + (gameCounter + 1) + ":</span>"));
 
       setTimeout(function() {
         tickTutorial(exampleGames, gameCounter, moveCounter);
       }, tickMs)
-    }, tickMs * 3)
+    }, tickMs)
   } else {
     setTimeout(function() {
       tickTutorial(exampleGames, gameCounter, moveCounter);
@@ -41,7 +46,7 @@ var executeTutorialStep = function(delta) {
   var piece = findPieceByPosition(origin);
 
   piece.highlight(true);
-  piece.delayedMove(target, tickMs / 2);
+  piece.delayedMove(target, tickMs / 3);
 };
 
 var findPieceByPosition = function(position) {
@@ -50,50 +55,123 @@ var findPieceByPosition = function(position) {
   });
 };
 
-var exampleGameQuickBlueScissorsWin = [
-  "h_i",
-  "r_q",
-  "b_f",
-  "q_m",
-  "i_e",
-  "m_i"
-];
+var publishTutorialMessage = function(message) {
+  if(!message) {
+    return;
+  }
+  var messageNode = buildMessage(message);
 
-var exampleGameMediumRedRockWin = [
-  "b_f",
-  "r_q",
-  "a_e",
-  "q_m",
-  "e_i",
-  "m_t",
-  "i_p",
-  "t_u",
-  "f_m",
-  "x_w",
-  "p_q"
-]
+  setTimeout(function() {
+    displayMessage(messageNode);
+  }, tickMs / 3);
+};
 
-var exampleGameLongMirrorOpeningRedWin = [
-  "a_e",
-  "y_u",
-  "h_l",
-  "x_t",
-  "b_i",
-  "r_q",
-  "i_p",
-  "t_x",
-  "e_i",
-  "u_t",
-  "p_o",
-  "x_u",
-  "i_m",
-  "q_r",
-  "l_p",
-  "t_x",
-  "p_t",
-  "x_y",
-  "t_x"
-];
+var displayMessage = function(messageNode) {
+  $(".tutorial-messages").append(messageNode)
+  $(".tutorial-messages").animate({scrollTop: $('.tutorial-messages').prop("scrollHeight")}, 500)
+}
+
+var buildMessage = function(message) {
+  var $el = $("<li/>", {
+    class: "message"
+  });
+
+  $el.html(message);
+  return $el;
+};
+
+var exampleGameQuickBlueScissorsWin = {
+  moves: [
+    "h_i",
+    "r_q",
+    "b_f",
+    "q_m",
+    "i_e",
+    "m_i"
+  ],
+  messages: [
+    "",
+    "",
+    "",
+    "<span class='blue'>Blue Scissors</span> puts <span class='red'>Red Paper</span> in check",
+    "",
+    "<span class='blue'>Blue Scissors</span> puts <span class='red'>Red Paper</span> in checkmate!",
+  ]
+};
+
+var exampleGameMediumRedRockWin = {
+  moves: [
+    "b_f",
+    "r_q",
+    "a_e",
+    "q_m",
+    "e_i",
+    "m_t",
+    "i_p",
+    "t_u",
+    "f_m",
+    "x_w",
+    "p_q"
+  ],
+  messages: [
+    "",
+    "",
+    "",
+    "",
+    "<span class='red'>Red Rock</span> puts <span class='blue'>Blue Scissors</span> in check",
+    "",
+    "<span class='red'>Red Rock</span> puts <span class='blue'>Blue Scissors</span> in check",
+    "",
+    "",
+    "<span class='blue'>Blue Paper</span> puts <span class='red'>Red Rock</span> in check",
+    "<span class='red'>Red Rock</span> puts <span class='blue'>Blue Scissors</span> in checkmate!",
+  ]
+};
+
+var exampleGameLongMirrorOpeningRedWin = {
+  moves: [
+    "a_e",
+    "y_u",
+    "h_l",
+    "x_t",
+    "b_i",
+    "r_q",
+    "i_p",
+    "t_x",
+    "e_i",
+    "u_t",
+    "p_o",
+    "x_u",
+    "i_m",
+    "q_r",
+    "l_p",
+    "t_x",
+    "p_t",
+    "x_y",
+    "t_x"
+  ],
+  messages: [
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "<span class='red'>Red Scissors</span> puts <span class='blue'>Blue Paper</span> in check",
+    "",
+    "",
+    "<span class='blue'>Blue Rock</span> puts <span class='red'>Red Scissors</span> in check",
+    "",
+    "",
+    "<span class='red'>Red Rock</span> puts <span class='blue'>Blue Scissors</span> in check",
+    "",
+    "<span class='red'>Red Paper</span> puts <span class='blue'>Blue Rock</span> in check",
+    "",
+    "<span class='red'>Red Paper</span> puts <span class='blue'>Blue Rock</span> in check",
+    "",
+    "<span class='red'>Red Paper</span> puts <span class='blue'>Blue Rock</span> in checkmate!",
+  ]
+};
 
 var exampleGames = [
   exampleGameQuickBlueScissorsWin,
